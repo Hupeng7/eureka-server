dc3a7a11a0d2d4801f8abefcdbef1d1f4ad1e877
wangxingjian2004 <wangxingjian1984@163.com>
2018-01-26 17:59:34 +0800

    第一次提交

---
+++ src/main/java/com/hzphfin/infra/eureka/eureka/server/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.hzphfin.infra.eureka.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+public class EurekaApplication {
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+	
+}
